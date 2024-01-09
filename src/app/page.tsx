1d4fe6943ccd51840bebc93adfbafd75b68ee0e5
okanay <okanay@hotmail.com>
2024-01-09 09:08:38 +0300

    filter-3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -154,7 +154,7 @@ export default function Home() {
           )}
 
           <Button type={"submit"} disabled={status === "loading"}>
-            Filter File By Keyword.
+            Filter File By Keywords.
           </Button>
         </form>
         <Button
