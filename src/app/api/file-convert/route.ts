32a03bb150df60a8479390dd561d9bdf66dded89
okanay <okanay@hotmail.com>
2024-01-09 07:26:49 +0300

    blob-2

--- src/app/api/file-convert/route.ts
+++ src/app/api/file-convert/route.ts
@@ -2,27 +2,31 @@ import { NextResponse } from "next/server";
 import { put } from "@vercel/blob";
 
 export async function POST(request: Request) {
-  const data = await request.formData();
-  const file: File | null = data.get("file") as unknown as File;
-  const keywords: string | null = data.get("keywords") as string;
-  const nameOption: string | null = data.get("nameOption") as string;
-  const customName: string | null = data.get("customName") as string;
+  try {
+    const data = await request.formData();
+    const file: File | null = data.get("file") as unknown as File;
+    const keywords: string | null = data.get("keywords") as string;
+    const nameOption: string | null = data.get("nameOption") as string;
+    const customName: string | null = data.get("customName") as string;
 
-  if (!file && !keywords) {
+    if (!file && !keywords) {
+      return NextResponse.json({ success: false });
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const value = buffer.toString("utf8").split("\n");
+    const filter = value.filter((item) => item.includes(keywords));
+    const join = filter.join("\n");
+    const fileName = createFileName(file.name, nameOption, customName);
+
+    const { url } = await put(fileName, join, { access: "public" });
+    return NextResponse.json({ success: true, url });
+  } catch (e) {
+    console.log(e);
     return NextResponse.json({ success: false });
   }
-
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-
-  const value = buffer.toString("utf8").split("\n");
-  const filter = value.filter((item) => item.includes(keywords));
-  const join = filter.join("\n");
-  const fileName = createFileName(file.name, nameOption, customName);
-
-  const { url } = await put(fileName, join, { access: "public" });
-  console.log(url);
-  return NextResponse.json({ success: true, url });
 }
 
 const createFileName = (
