b395053d16de483637c4a875006e5a8751f4d40d
okanay <okanay@hotmail.com>
2024-01-11 11:43:17 +0300

    new feature add

--- src/components/search-form/form-options.tsx
+++ src/components/search-form/form-options.tsx
@@ -4,6 +4,8 @@ import { CustomMaxLength } from "@/components/search-form/custom-max-length";
 import { SuccessMessage } from "@/components/search-form/success-message";
 import { CustomNameOption } from "@/components/search-form/custom-name-option";
 import { CustomCalendarOption } from "@/components/search-form/custom-calendar-option";
+import { Label } from "@/components/ui/label";
+import { WarningMessage } from "@/components/search-form/warning-message";
 
 export const FormOptions = () => {
   return (
@@ -11,18 +13,17 @@ export const FormOptions = () => {
       <h1 className="text-lg font-semibold tracking-wide text-gray-800">
         Options.
       </h1>
-      <div className="flex w-fit flex-wrap items-start justify-start gap-12">
-        <div className={"flex flex-col flex-wrap gap-4"}>
-          <CustomMaxLength />
-        </div>
-        <div className={"flex flex-col flex-wrap gap-6"}>
-          <div className={"flex flex-col flex-wrap gap-4"}>
-            <CustomNameOption />
-          </div>
-          <div className={"flex flex-col flex-wrap gap-4"}>
-            <CustomCalendarOption />
-            <SuccessMessage />
-          </div>
+      <div className={"flex flex-col gap-2"}>
+        <Label>Customize Filter Type.</Label>
+        <WarningMessage />
+      </div>
+      <div className="flex w-fit flex-col flex-wrap items-start justify-start gap-8">
+        <CustomNameOption />
+        <CustomMaxLength />
+        <CustomCalendarOption />
+        <div className={"flex flex-col gap-2"}>
+          <Label>Customize Time.</Label>
+          <WarningMessage />
         </div>
       </div>
     </div>
