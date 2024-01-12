14db7a527a55eb640b1d9015a4b982788ccd6d2b
okanay <okanay@hotmail.com>
2024-01-12 11:01:16 +0300

    css fix 2

--- src/components/search-form/form-options.tsx
+++ src/components/search-form/form-options.tsx
@@ -4,28 +4,30 @@ import { CustomMaxLength } from "@/components/search-form/radio-options/custom-m
 import { SuccessMessage } from "@/components/search-form/messages/success-message";
 import { CustomNameOption } from "@/components/search-form/radio-options/custom-name-option";
 import { CustomCalendarOption } from "@/components/search-form/radio-options/custom-calendar-option";
-import { Label } from "@/components/ui/label";
-import { WarningMessage } from "@/components/search-form/messages/warning-message";
 import { CustomFilterOption } from "@/components/search-form/radio-options/custom-filter-option";
 import { CustomDatetimeOption } from "@/components/search-form/radio-options/custom-datetime-option";
 
 export const FormOptions = () => {
   return (
-    <div className="mx-auto flex w-fit flex-col gap-6 px-4 sm:px-0">
+    <div className="mx-auto flex w-fit flex-col items-center gap-6 px-4 sm:items-start sm:px-0">
       <h1 className="text-lg font-semibold tracking-wide text-gray-800">
         Options.
       </h1>
-      <div className="flex w-fit flex-col flex-wrap items-start justify-start gap-8">
-        <CustomNameOption />
-        <div className={"flex flex-col gap-2"}>
-          <CustomFilterOption />
-          <SuccessMessage />
+      <div className="flex flex-wrap items-center justify-center gap-8 sm:flex-nowrap sm:items-start sm:justify-start">
+        <div className="flex w-fit flex-col flex-wrap items-start justify-start gap-8">
+          <CustomCalendarOption />
+          <div className={"flex flex-col gap-2"}>
+            <CustomDatetimeOption />
+            <SuccessMessage />
+          </div>
+          <CustomMaxLength />
         </div>
-        <CustomMaxLength />
-        <CustomCalendarOption />
-        <div className={"flex flex-col gap-2"}>
-          <CustomDatetimeOption />
-          <SuccessMessage />
+        <div className="flex w-fit flex-col flex-wrap items-start justify-start gap-8">
+          <CustomNameOption />
+          <div className={"flex flex-col gap-2"}>
+            <CustomFilterOption />
+            <SuccessMessage />
+          </div>
         </div>
       </div>
     </div>
